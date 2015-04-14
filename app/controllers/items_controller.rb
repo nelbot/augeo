fc18c7621720fd5795f9f5cda031ce9a11601566
will love <wlove29@gmail.com>
2015-04-14 11:28:10 -0400

    added post for items new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,17 @@ class ItemsController < ApplicationController
 		@user = User.find(session[:id])
 		@item.user_id = session[:id]
 		@item.save
+		@items = Item.all
 		render 'new'
 	end
 
 	def create
 		@item = Item.create items_privates
-
+		redirect_to new_user_item_path	
 	end
+
+
+
 
 	private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
 	belongs_to :user
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :presence, presence: true
+	validates :price, presence: true
 end
